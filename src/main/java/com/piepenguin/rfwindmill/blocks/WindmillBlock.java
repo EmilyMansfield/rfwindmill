6b2ff3db9b21196c67bf6979d58307f5c82a3983
Daniel Mansfield <daniel.mansfield99@gmail.com>
2015-04-21 15:55:11 +0100

    Added new textures

--- src/main/java/com/piepenguin/rfwindmill/blocks/WindmillBlock.java
+++ src/main/java/com/piepenguin/rfwindmill/blocks/WindmillBlock.java
@@ -6,8 +6,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 public class WindmillBlock extends Block implements ITileEntityProvider {
@@ -17,6 +19,7 @@ public class WindmillBlock extends Block implements ITileEntityProvider {
     private int capacity;
 
     private String name;
+    private IIcon[] icons = new IIcon[6];
 
     public WindmillBlock(String pName, int pMaximumEnergyGeneration, int pCapacity) {
         super(Material.rock);
@@ -26,8 +29,22 @@ public class WindmillBlock extends Block implements ITileEntityProvider {
         name = pName;
         this.setBlockName(Constants.MODID + "_" + name);
         this.setCreativeTab(CreativeTabs.tabBlock);
-        this.setBlockTextureName(Constants.MODID + ":" + name);
         GameRegistry.registerBlock(this, name);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister iconRegister) {
+        icons[0] = iconRegister.registerIcon(Constants.MODID + ":" + name + "Side");
+        icons[1] = iconRegister.registerIcon(Constants.MODID + ":" + name + "Side");
+        icons[2] = iconRegister.registerIcon(Constants.MODID + ":" + name + "Front");
+        icons[3] = iconRegister.registerIcon(Constants.MODID + ":" + name + "Front");
+        icons[4] = iconRegister.registerIcon(Constants.MODID + ":" + name + "Side");
+        icons[5] = iconRegister.registerIcon(Constants.MODID + ":" + name + "Side");
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta) {
+        return icons[side];
     }
 
     @Override
