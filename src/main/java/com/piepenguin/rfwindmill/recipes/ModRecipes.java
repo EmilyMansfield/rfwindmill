f6fb61ef73005a2e998538c1379714bccbe30748
Daniel Mansfield <daniel.mansfield99@gmail.com>
2015-04-23 10:58:55 +0100

    Made vanilla windmill use tin if Thermal Foundation is installed

--- src/main/java/com/piepenguin/rfwindmill/recipes/ModRecipes.java
+++ src/main/java/com/piepenguin/rfwindmill/recipes/ModRecipes.java
@@ -8,6 +8,7 @@ import com.piepenguin.rfwindmill.lib.Constants;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class ModRecipes {
@@ -26,12 +27,21 @@ public class ModRecipes {
     }
 
     public static void registerVanillaRecipes() {
-        GameRegistry.addRecipe(new ShapedOreRecipe(ModBlocks.windmillBlock1, " x ", "ibi", " r ", 'x', ModItems.rotor1, 'i', "ingotIron", 'r', "dustRedstone", 'b', "blockIron"));
+        String ingotTin = getOreWithFallback("ingotTin", "ingotIron");
+
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModBlocks.windmillBlock1, " x ", "ibi", " r ", 'x', ModItems.rotor1, 'i', ingotTin, 'r', "dustRedstone", 'b', "blockIron"));
     }
 
     public static void registerThermalExpansionRecipes() {
         ItemStack powerCoilSilver = Preconditions.checkNotNull(GameRegistry.findItemStack(Constants.THERMAL_EXPANSION_MOD_ID, "powerCoilSilver", 1));
         ItemStack machineFrameBasic = Preconditions.checkNotNull(GameRegistry.findItemStack(Constants.THERMAL_EXPANSION_MOD_ID, "frameMachineBasic", 1));
         GameRegistry.addRecipe(new ShapedOreRecipe(ModBlocks.windmillBlock1, " x ", "gmg", " c ", 'x', ModItems.rotor1, 'g', "gearTin", 'm', machineFrameBasic, 'c', powerCoilSilver));
+    }
+
+    public static String getOreWithFallback(String ore, String fallback) {
+        if(OreDictionary.getOres(ore).isEmpty()) {
+            return fallback;
+        }
+        return ore;
     }
 }
