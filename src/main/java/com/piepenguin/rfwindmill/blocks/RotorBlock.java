0c5ce4c9b7055347a5839379013bbbbbbcceb124
Daniel Mansfield <daniel.mansfield99@gmail.com>
2015-04-29 15:04:38 +0100

    Rotor is now dropped when broken and turbine is alerted

--- src/main/java/com/piepenguin/rfwindmill/blocks/RotorBlock.java
+++ src/main/java/com/piepenguin/rfwindmill/blocks/RotorBlock.java
@@ -1,12 +1,18 @@
 package com.piepenguin.rfwindmill.blocks;
 
+import com.piepenguin.rfwindmill.items.ModItems;
 import com.piepenguin.rfwindmill.lib.Constants;
+import com.piepenguin.rfwindmill.lib.EnergyStorage;
 import com.piepenguin.rfwindmill.lib.Util;
 import com.piepenguin.rfwindmill.tileentities.TileEntityRotorBlock;
+import com.piepenguin.rfwindmill.tileentities.TileEntityWindmillBlock;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
@@ -38,6 +44,25 @@ public class RotorBlock extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World pWorld, int pMeta) {
         return new TileEntityRotorBlock();
+    }
+
+    @Override
+    public void onBlockHarvested(World pWorld, int pX, int pY, int pZ, int pSide, EntityPlayer pPlayer) {
+        // Tell the parent windmill block that it no longer has a rotor
+        TileEntityRotorBlock rotorEntity = (TileEntityRotorBlock)pWorld.getTileEntity(pX, pY, pZ);
+        ForgeDirection rotorDir = Util.intToDirection(rotorEntity.getBlockMetadata()).getOpposite();
+        int parentX = pX + rotorDir.offsetX;
+        int parentY = pY + rotorDir.offsetY;
+        int parentZ = pZ + rotorDir.offsetZ;
+        TileEntityWindmillBlock windmillEntity = (TileEntityWindmillBlock)pWorld.getTileEntity(parentX, parentY, parentZ);
+        windmillEntity.setRotor(false);
+        // Drop the item
+        ItemStack itemStack = new ItemStack(ModItems.rotor1);
+        pWorld.setBlockToAir(pX, pY, pZ);
+        EntityItem entityItem = new EntityItem(pWorld, pX + 0.5, pY + 0.5, pZ + 0.5, itemStack);
+        entityItem.motionX = 0;
+        entityItem.motionZ = 0;
+        pWorld.spawnEntityInWorld(entityItem);
     }
 
     public static boolean canPlace(World pWorld, int pX, int pY, int pZ, EntityPlayer pPlayer, ForgeDirection pDir ) {
