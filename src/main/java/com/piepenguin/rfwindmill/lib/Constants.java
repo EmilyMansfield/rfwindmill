bf94c6ee7ef43c5c05590360efc04ea2c1e852ea
Daniel Mansfield <daniel.mansfield99@gmail.com>
2015-05-06 17:31:16 +0100

    Updated for v0.5

--- src/main/java/com/piepenguin/rfwindmill/lib/Constants.java
+++ src/main/java/com/piepenguin/rfwindmill/lib/Constants.java
@@ -3,7 +3,7 @@ package com.piepenguin.rfwindmill.lib;
 public class Constants {
     public static final String MODID = "rfwindmill";
     public static final String NAME = "RFWindmill";
-    public static final String VERSION = "0.42";
+    public static final String VERSION = "0.5";
     public static final String CLIENT_PROXY_CLASS = "com.piepenguin.rfwindmill.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.piepenguin.rfwindmill.proxy.CommonProxy";
     public static final String THERMAL_EXPANSION_MOD_ID = "ThermalExpansion";
