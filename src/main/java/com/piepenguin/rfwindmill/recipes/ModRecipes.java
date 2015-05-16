3a0061ec60c271ab14d27edbca0c3783c22ff1c5
Daniel Mansfield <daniel.mansfield99@gmail.com>
2015-05-16 22:45:49 +0100

    Added Javadoc to the recipes

--- src/main/java/com/piepenguin/rfwindmill/recipes/ModRecipes.java
+++ src/main/java/com/piepenguin/rfwindmill/recipes/ModRecipes.java
@@ -14,8 +14,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
+/**
+ * Contains methods to register the recipes for all the items and blocks in the
+ * mod. Uses vanilla recipes by default but if Thermal Foundation or Thermal
+ * Expansion is loaded (and the functionality not disabled in the config file)
+ * then the recipes will be modified to utilise those mods.
+ */
 public class ModRecipes {
 
+    /**
+     * Register Thermal Expansion and Thermal Foundation recipes if they are
+     * loaded and the functionality is enabled in the config file otherwise
+     * register the vanilla recipes.
+     */
     public static void init() {
         registerCommonRecipes();
         if(Util.useThermalFoundation() && !ModConfiguration.useVanillaRecipes()) {
@@ -32,10 +43,19 @@ public class ModRecipes {
         }
     }
 
+    /**
+     * Register recipes not dependent on the loaded mods.
+     */
     private static void registerCommonRecipes() {
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor1, "i  ", "iii", "  i", 'i', "ingotIron"));
     }
 
+    /**
+     * Register vanilla turbine recipes, utilising Forge ore dictionary materials
+     * if relevant and loaded, otherwise recipes fall back to pure vanilla.
+     * Tasty. Called when Thermal Expansion isn't installed or vanilla recipes
+     * are forced.
+     */
     private static void registerVanillaTurbines() {
         String ingotTin = getOreWithFallback("ingotTin", "ingotIron");
 
@@ -50,20 +70,38 @@ public class ModRecipes {
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.windmillBlock, 1, 3), "d d", " b ", "i i", 'i', "ingotGold", 'd', "gemDiamond", 'b', new ItemStack(ModBlocks.windmillBlock, 1, 2)));
     }
 
-    // No Thermal Expansion so use gold rotor, nether rotor, and diamond rotor
+    /**
+     * Register vanilla rotor recipes. Called when Thermal Foundation is not
+     * loaded or vanilla recipes are forced. Rotor types created are
+     * <ul>
+     *     <li>Gold Rotor</li>
+     *     <li>Nether Rotor</li>
+     *     <li>Diamond Rotor</li>
+     * </ul>
+     */
     private static void registerVanillaRotors() {
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor2, "g  ", "gig", "  g", 'i', "ingotIron", 'g', "ingotGold"));
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor3, "n  ", "non", "  n", 'n', Items.quartz, 'o', Blocks.obsidian));
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor4, "d  ", "dgd", "  d", 'd', "gemDiamond", 'g', "ingotGold"));
     }
 
-    // Thermal Foundation loaded so use electrum rotor, signalum rotor, and enderium rotor
+    /**
+     * Register Thermal Foundation rotor recipes. Rotor types are
+     * <ul>
+     *     <li>Electrum Rotor</li>
+     *     <li>Signalum Rotor</li>
+     *     <li>Enderium Rotor</li>
+     * </ul>
+     */
     private static void registerThermalFoundationRotors() {
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor2, "e  ", "eie", "  e", 'i', "ingotInvar", 'e', "ingotElectrum"));
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor3, "s  ", "ses", "  s", 's', "ingotSignalum", 'e', "ingotElectrum"));
         GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.rotor4, "e  ", "eie", "  e", 'e', "ingotEnderium", 'i', "ingotInvar"));
     }
 
+    /**
+     * Register Thermal Expansion turbine recipes.
+     */
     private static void registerThermalExpansionTurbines() {
         ItemStack powerCoilSilver = Preconditions.checkNotNull(GameRegistry.findItemStack(Constants.THERMAL_EXPANSION_MOD_ID, "powerCoilSilver", 1));
         ItemStack machineFrameBasic = Preconditions.checkNotNull(GameRegistry.findItemStack(Constants.THERMAL_EXPANSION_MOD_ID, "frameMachineBasic", 1));
@@ -83,6 +121,13 @@ public class ModRecipes {
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.windmillBlock, 1, 3), "igi", "jmj", "i i", 'i', "ingotSilver", 'j', "ingotPlatinum", 'g', "gearEnderium", 'm', new ItemStack(ModBlocks.windmillBlock, 1, 2)));
     }
 
+    /**
+     * Attempt to find the {@code pOre} in the Forge ore dictionary and return
+     * it if found otherwise return the {@code pFallback}
+     * @param pOre Ore name to search for (not necessarily an ore)
+     * @param pFallback Material to fall back to if the {@code pOre} wasn't found
+     * @return {@code pOre} if it's in the dictionary, {@code fallback} otherwise
+     */
     public static String getOreWithFallback(String pOre, String pFallback) {
         if(OreDictionary.getOres(pOre).isEmpty()) {
             return pFallback;
